4946502e99bbb9db41c3eb1584a6bafbaecb15b3
Neority <Tyuiu.StoyakinNM@outlook.com>
2024-10-31 16:22:03 +0300

    Task 1

---
+++ Tyuiu.StoyakinNM.Sprint4.Task1.V6.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Tyuiu.StoyakinNM.Sprint4.Task1.V6.Lib;
+namespace Tyuiu.StoyakinNM.Sprint4.Task1.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 2, 4, 4, 7, 2, 5, 4, 3, 3, 4 };
+
+            int res = ds.Calculate(array);
+            int wait = 1024;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.StoyakinNM.Sprint4.Task1.V6/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.StoyakinNM.Sprint4.Task1.V6.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #4 | Выполнил: Стоякин Н.М. | ПКТб-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерные массивы. (ввод с клавиатуры)                           *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнил: Стоякин Никита Михайлович | ПКТб 24-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+        Console.WriteLine("* значениями с клавиатуры в диапазоне от 2 до 7 подсчитать произведение   *");
+        Console.WriteLine("* четных элементов массива.  С клавиатуры: 2, 4, 4, 7, 2, 5, 4, 3, 3, 4   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int len;
+        Console.WriteLine("Введите количество элементов массива: ");
+        len = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[len];
+
+        for (int i = 0; i <= len-1; i++)
+        {
+            Console.Write("Введите значение " + i + " элемента массива");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine();
+        Console.WriteLine("Массив: ");
+        for (int i = 0;i <= len-1;i++)
+        {
+            Console.Write(array[i] + "\t");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ;                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int res = ds.Calculate(array);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
